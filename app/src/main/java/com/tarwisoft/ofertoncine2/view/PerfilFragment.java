0e88847a089827bc1273cd4678cdee88090e0146
Jhovny Flores Ninaco <jfloresninaco@hotmail.com>
2019-04-24 18:14:11 -0500

    Consumir servicio  rest de cines

--- app/src/main/java/com/tarwisoft/ofertoncine2/view/PerfilFragment.java
+++ app/src/main/java/com/tarwisoft/ofertoncine2/view/PerfilFragment.java
@@ -3,13 +3,20 @@ package com.tarwisoft.ofertoncine2.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 import com.tarwisoft.ofertoncine2.LoginActivity;
 import com.tarwisoft.ofertoncine2.R;
 
@@ -18,6 +25,7 @@ import com.tarwisoft.ofertoncine2.R;
  */
 public class PerfilFragment extends Fragment {
 
+    private EditText txtToken;
 
     private Button btnIngresar;
 
@@ -34,10 +42,33 @@ public class PerfilFragment extends Fragment {
         View view =inflater.inflate(R.layout.fragment_perfil, container, false);
 
         btnIngresar=(Button) view.findViewById(R.id. btnIngresar);
+        txtToken=(EditText) view.findViewById(R.id. txtToken);
 
         btnIngresar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0)
             {
+
+                FirebaseInstanceId.getInstance().getInstanceId()
+                        .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                                if (!task.isSuccessful()) {
+                                    Log.w("TAG", "getInstanceId failed", task.getException());
+                                    return;
+                                }
+
+                                // Get new Instance ID token
+                                String token = task.getResult().getToken();
+
+                                // Log and toast
+
+                                txtToken.setText(token);
+
+                            }
+                        });
+
+
+                /*
                 //Creamos el Intent
                 Intent intent =
                         new Intent((getActivity()), LoginActivity.class);
@@ -51,6 +82,7 @@ public class PerfilFragment extends Fragment {
 
                 //Iniciamos la nueva actividad
                 startActivity(intent);
+                */
             }
         });
 
